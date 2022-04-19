0ddcc76cbe4fce0a256fedd71658a9eae6b5539e
SzymonJag15 <69639056+SzymonJag15@users.noreply.github.com>
2022-04-19 16:32:04 +0200

    Task/second task (#11)
    * Init (#8)
    
    Co-authored-by: szymon <szymon.jagielski@olicom.pl>
    
    * Create views without logic (#9)
    
    Co-authored-by: szymon <szymon.jagielski@olicom.pl>
    
    * Add conf
    
    Co-authored-by: szymon <szymon.jagielski@olicom.pl>

---
+++ frontend/task-2/src/components/AppSelect/AppSelect.vue
@@ -0,0 +1,22 @@
+<template>
+  <select
+    class="app-select"
+    @change="$emit('update:modelValue', $event.target.value)"
+  >
+    <option v-for="option in options" :key="option" :value="option">
+      {{ option }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "AppSelect",
+  props: {
+    id: String,
+    options: Array,
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" scoped />
