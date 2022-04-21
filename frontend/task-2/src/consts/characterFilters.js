7294140f8fe697c5ac56b5782a109b67c663c0e0
szymon <szymon.jagielski@olicom.pl>
2022-04-21 16:13:46 +0200

    Add functionality

---
+++ frontend/task-2/src/consts/characterFilters.js
@@ -0,0 +1,33 @@
+export const allCharactersNames = [
+  {
+    name: "Wszystkie postaci",
+    value: "all",
+  },
+  {
+    name: "Tylko uczniowie",
+    value: "hogwartsStudent",
+  },
+  {
+    name: "Tylko kadra nauczycielska",
+    value: "hogwartsStaff",
+  },
+];
+
+export const allCharactersHouses = [
+  {
+    name: "Wszystkie domy",
+    value: "all",
+  },
+  {
+    name: "Gryffindor",
+    value: "Gryffindor",
+  },
+  {
+    name: "Slytherin",
+    value: "Slytherin",
+  },
+  {
+    name: "Ravenclaw",
+    value: "Ravenclaw",
+  },
+];
